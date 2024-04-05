50a9672caf46329027cd93f282f1b64cf8be2700
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-04-04 21:00:00 -0300

    .

---
+++ projeto-angular/src/app/meu-componente/meu-componente.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+CommonModule
+
+@Component({
+  selector: 'app-meu-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './meu-componente.component.html',
+  styleUrl: './meu-componente.component.css'
+})
+export class MeuComponenteComponent {
+
+}
---
+++ tipos.ts
@@ -0,0 +1,10 @@
+const valor:number = 10;
+const palavra:string ="hello";
+const bool:boolean =true;
+const valores:number[] = [1,2,3,4,5];
+const pessoa: {nome: string, idade: number} = {
+    nome: "thiago",
+    idade: 26
+}
+
+    
